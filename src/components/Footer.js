6c316b95acffa35c7273f2eb42e1022237920134
Aniket <rautaniket27@gmail.com>
2024-04-13 01:24:59 +0530

    minor footer style fixes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,15 @@
 import '../common.css';
-import insta from '../assets/icons/insta.png';
-import tiktok from '../assets/icons/tiktok.png';
-import linkedin from '../assets/icons/linedin.png';
-import pinterest from '../assets/icons/pinterest.png';
-import youtube from '../assets/icons/yt.png';
-import x from '../assets/icons/x.png';
+import insta from '../assets/icons/Socials_1.svg';
+import tiktok from '../assets/icons/Socials_4.svg';
+import linkedin from '../assets/icons/Socials_3.svg';
+import pinterest from '../assets/icons/Socials_2.svg';
+import youtube from '../assets/icons/Socials_5.svg';
+import x from '../assets/icons/Socials.svg';
 import saasLogo from '../assets/icons/logosaas.png'
 
 export const Footer=()=>{
     return(
-        <footer className="responsive-container space-between pad-16-58">
+        <footer className="responsive-container space-between pad-16-58 mobile-flex">
             <div className='font-14'>@ 2024 Your Company, Inc. All rights reserved</div>
             <div className='footer-connections'>
                 <img src={x}></img>
